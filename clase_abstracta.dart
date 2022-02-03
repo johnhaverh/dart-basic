ab512211e4789e17cdde6ad8b527920a1646b238
John Haver Herrera <johnhaverh@gmail.com>
2022-02-03 12:03:47 -0500

    clase_abstracta.dart: create

---
+++ clase_abstracta.dart
@@ -0,0 +1,41 @@
+void main() {
+  final car = new Car();
+  final motorcycle = new Motorcycle();
+
+  VehicleBrand(car);
+  VehicleBrand(motorcycle);
+}
+
+void VehicleBrand(Vehicle vehicle) {
+  vehicle.setBrand();
+}
+
+abstract class Vehicle {
+  int? wheels;
+  String? typeEngine;
+  int? passagers;
+  String? brand;
+
+  void setBrand();
+}
+
+class Car implements Vehicle {
+  int? wheels;
+  String? typeEngine;
+  int? passagers;
+  String? brand;
+  int? doors;
+  
+  //Car({required this.brand});
+
+  void setBrand() => print("Ferrari");
+}
+
+class Motorcycle implements Vehicle {
+  int? wheels;
+  String? typeEngine;
+  int? passagers;
+  String? brand;
+
+  void setBrand() => print('Honda');
+}
